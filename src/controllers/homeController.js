ed735899ae27f032e92d336148270ce12241e05a
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2025-07-30 19:29:07 +0300

    check for render deployment

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -6,7 +6,7 @@ exports.index = (req, res) => {
   res.send(`
     <html>
       <body>
-        <h3>Click to open Swagger Docs:</h3>
+        <h3>Click to open Swagger Docs!!!:</h3>
         <a href=${serverUrl} target="_blank">
           Open Swagger Documentation
         </a>
